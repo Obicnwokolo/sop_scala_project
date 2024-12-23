36188e37aa2531b0920a4b2bd499d3ce010eb64b
Obicnwokolo <nwokolo.obinna10@gmail.com>
2024-12-23 10:10:04 +0000

    commit1

--- src/main/scala/spark/finance.scala
+++ src/main/scala/spark/finance.scala
@@ -1,5 +1,45 @@
 package spark
 
-object finance {
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
+object finance {
+  def main(args: Array[String]): Unit = {
+    // Create Spark session
+    val spark = SparkSession.builder()
+      .appName("CSV to PostgresDb")
+      .master("local[*]")
+      .getOrCreate()
+
+    // Define the file path
+    val csvFilePath = "C:\\Users\\chigb\\Downloads\\financial_data.csv"
+
+    // Read CSV file into a DataFrame
+    val df = spark.read
+      .option("header", "true") // Use the first row as header
+      .option("inferSchema", "true") // Infer data types
+      .csv(csvFilePath)
+
+    // Show the DataFrame (optional)
+    df.show(5)
+
+    // Define JDBC connection parameters
+    val jdbcUrl = "jdbc:postgresql://18.132.73.146:5432/testdb"
+    val dbProperties = new java.util.Properties()
+    dbProperties.setProperty("user", "consultants")  // Your database username
+    dbProperties.setProperty("password", "WelcomeItc@2022")  // Your database password
+    dbProperties.setProperty("driver", "org.postgresql.Driver")
+
+    val dbTable = "sop_finace_scala"
+
+
+    // Write DataFrame to PostgreSQL
+    df.write
+      .mode("overwrite") // Options: overwrite, append, ignore, error
+      .jdbc(jdbcUrl, dbTable, connectionProperties)
+
+    // Stop SparkSession
+    spark.stop()
+  }
 }
