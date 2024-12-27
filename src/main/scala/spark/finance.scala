f24e4fd0767776f5078013e993b9606ad726c5d9
Obicnwokolo <nwokolo.obinna10@gmail.com>
2024-12-27 17:43:42 +0000

    incremental load

--- src/main/scala/spark/finance.scala
+++ src/main/scala/spark/finance.scala
@@ -33,13 +33,33 @@ object finance {
     dbProperties.setProperty("password", "WelcomeItc@2022")  // Your database password
     dbProperties.setProperty("driver", "org.postgresql.Driver")
 
+    println("read successful")
+    //df.show(5)
+    val totalRows = df.count()
+    println(s"Total number of rows: $totalRows")
 
+    // Retrieve the maximum id from the database
+    val maxIdQuery = "(SELECT MAX(id) AS max_id FROM sop_fraud_scala) AS temp_table"
+    val maxIdDf = spark.read.jdbc(jdbcUrl, maxIdQuery, dbProperties)
+    val maxId = maxIdDf.first().getAs[Number]("max_id").longValue()
 
+    //------------------------------------------------------------------------------------------------
+    // Filter the DataFrame
+    val filteredDf = df.filter(df("id") > maxId)
+    //filteredDf.show(5)
+    val totalRows2 = filteredDf.count()
+    println(s"new Total number of rows: $totalRows2")
+    //-----------------------------------------------------------------------------------------------
+
+    //filteredDf.write.mode("append").csv("C:\\Users\\chigb\\Downloads\\outputs")
 
     // Write DataFrame to PostgreSQL
-    df.write
-      .mode("overwrite") // Options: overwrite, append, ignore, error
+    filteredDf.write //
+      .mode("append") // Options: overwrite, append, ignore, error
       .jdbc(jdbcUrl, dbTable, dbProperties)
+
+    println(s"$totalRows2 new records added successfully")
+
 
     // Stop SparkSession
     spark.stop()
